518153123f980a197a484c5a0abae917c960af86
Bhisma Chandra <bhisma.chandra.tif422@polban.ac.id>
2024-09-08 14:02:40 +0700

    CMS HTML
    menambahkan setiap halaman CMS html tailwindcss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+<div>
+  
+</div>
 (function () {
   "use strict";
 
@@ -156,3 +167,4 @@
   themeCheck();
   /* ========  themeSwitcher End ========= */
 })();
+
